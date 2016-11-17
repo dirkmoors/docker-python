bf290badc3b912d81f80e51c833acd3e2068a566
Dirk Moors <dirkmoors@gmail.com>
2016-11-17 15:01:47 +0100

    Added CHOWNDIRS env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@
 
 USER_ID=${LOCAL_USER_ID:-0}
 USERNAME=${LOCAL_USERNAME:-user}
+CHOWNDIRS=${CHOWNDIRS}
 
 if [ "${WAIT_TCP}" ];
 then
@@ -28,7 +29,19 @@ case ${USER_ID} in
         # Run as non-root
         adduser -s /bin/bash -u ${USER_ID} -D -h /home/${USERNAME} ${USERNAME}
         export HOME=/home/${USERNAME}
-        chown -R ${USER_ID}:${USER_ID} ${SRC_DIR}
+
+        # chown dirs
+        if [ "${CHOWNDIRS}" ];
+        then
+            IFS=',' read -r -a array <<< "${CHOWNDIRS}"
+            for element in "${array[@]}"
+            do
+                printf "transfering directory ownership for ${element} to ${USER_ID}:${USER_ID} ...\n"
+                chown -R ${USER_ID}:${USER_ID} ${element};
+            done
+        fi
+
+        # Exec statement
         exec su-exec ${USERNAME} "$@"
         ;;
 esac
