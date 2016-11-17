8d0856c2f4efb77b8d8ef5baaa93f17cb6bbe43a
Dirk Moors <dirkmoors@gmail.com>
2016-11-17 19:40:14 +0100

    Updated entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,13 +7,14 @@
 USER_ID=${LOCAL_USER_ID:-0}
 USERNAME=${LOCAL_USERNAME:-user}
 CHOWNDIRS=${CHOWNDIRS}
+VERBOSE=${VERBOSE}
 
 if [ "${WAIT_TCP}" ];
 then
     IFS=',' read -r -a array <<< "${WAIT_TCP}"
     for element in "${array[@]}"
     do
-        printf "waiting for open TCP port: ${element} ...\n"
+        if [ "${VERBOSE}" ]; then printf "waiting for open TCP port: ${element} ...\n"; fi
         IFS=':' read -r -a HOST_PORT <<< "${element}"
         waitforit -host=${HOST_PORT[0]} -port=${HOST_PORT[1]} -timeout=20;
     done
@@ -36,7 +37,7 @@ case ${USER_ID} in
             IFS=',' read -r -a array <<< "${CHOWNDIRS}"
             for element in "${array[@]}"
             do
-                printf "transfering directory ownership for ${element} to ${USER_ID}:${USER_ID} ...\n"
+                if [ "${VERBOSE}" ]; then printf "transfering directory ownership for ${element} to ${USER_ID}:${USER_ID} ...\n"; fi
                 chown -R ${USER_ID}:${USER_ID} ${element};
             done
         fi
