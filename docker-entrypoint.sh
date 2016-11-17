81f8d885cbd03f5c534c6d11f32e24592676ca1a
Dirk Moors <dirkmoors@gmail.com>
2016-11-17 12:11:35 +0100

    Implemented waitforit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,18 @@
 USER_ID=${LOCAL_USER_ID:-0}
 USERNAME=${LOCAL_USERNAME:-user}
 
+if [ "${WAIT_TCP}" ];
+then
+    IFS=',' read -r -a array <<< "${WAIT_TCP}"
+    for element in "${array[@]}"
+    do
+        printf "waiting for open TCP port: ${element} ...\n"
+        IFS=':' read -r -a HOST_PORT <<< "${element}"
+        waitforit -host=${HOST_PORT[0]} -port=${HOST_PORT[1]} -timeout=20;
+    done
+fi
+
+
 case ${USER_ID} in
    "0")
         # Run as root
